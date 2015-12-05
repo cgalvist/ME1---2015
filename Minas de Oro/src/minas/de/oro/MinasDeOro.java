08410bff5759ef5cfb15e0977c89eebb763a1cf2
cesar galvis triviño <cgalvist@gmail.com>
2015-12-05 15:24:30 -0500

    creación del proyecto en java

---
+++ Minas de Oro/src/minas/de/oro/MinasDeOro.java
@@ -0,0 +1,16 @@
+package minas.de.oro;
+
+/**
+ *
+ * @author cesar
+ */
+public class MinasDeOro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
